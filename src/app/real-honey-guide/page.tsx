6e8c00b7f4d0e090608f78f45a13cc017f3eef67
ILEMONAcodes <cryptoilemona@gmail.com>
2026-09-08 11:03:35 +0100

    Force dynamic rendering on real-honey-guide page

--- src/app/real-honey-guide/page.tsx
+++ src/app/real-honey-guide/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+
+import React from 'react';
 import { HeroTwo } from '@/components/free-ebook/HeroTwo';
 
 export default function RealHoneyPage() {
